07ac7859652bbb07cf47c3eb29605fefbbb115ad
Murray Smith <muzzdeni78@gmail.com>
2018-07-08 09:26:32 +1200

    Update of GeminiStateMachine.h

--- Gemini/Gemini.ino
+++ Gemini/Gemini.ino
@@ -6,11 +6,12 @@ void setup() {
   Serial.println("Gemini");
   //Setup Periphials
   //Initalise gemini executor
-  geminiFlightLogic();
-  Serial.println("Error Logic has unexpectidly quit\n restart arduino");
+ 
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+   geminiFlightLogic();
+   Serial.println("Error Logic has unexpectidly quit\n restarting logic");
 }
